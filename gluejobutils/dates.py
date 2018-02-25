65e6114a4c7f7bd560f12437d2caa26aa1c70145
Robin Linacre <robinlinacre@hotmail.com>
2018-02-25 09:37:32 +0000

    Update dates.py

--- gluejobutils/dates.py
+++ gluejobutils/dates.py
@@ -5,6 +5,6 @@ def convert_all_date_cols_to_string(df):
 
     date_cols = [f.name for f in df.schema if f.dataType.simpleString() == 'date']
     for dc in date_cols:
-        df = df.withColumn(dc, dfs[dc].cast('string'))
+        df = df.withColumn(dc, df[dc].cast('string'))
     
     return df
