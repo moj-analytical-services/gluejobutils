65244dabc1b3e2a56b84c51a7c7b1e0eb25c5de5
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2018-06-08 15:31:09 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+# see https://stackoverflow.com/questions/14399534/reference-requirements-txt-for-the-install-requires-kwarg-in-setuptools-setup-py
+setup(name='gluejobutils',
+      version='0.1',
+      description='Python 2.7 utils for glue jobs',
+      url='https://github.com/RobinL/gluejobutils'
+      author='Robin Linacre',
+      author_email='robinlinacre@hotmail.com',
+      license='MIT',
+      packages=['gluejobutils'],
+      zip_safe=False)
