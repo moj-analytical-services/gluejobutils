a55002419587b4a2c07d64695e98b0acafd81a1c
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2018-06-09 10:03:11 +0100

    removed circular dep

--- gluejobutils/s3.py
+++ gluejobutils/s3.py
@@ -1,5 +1,7 @@
-from gluejobutils.json_funcs import read_json_from_s3
 from gluejobutils.datatypes import translate_metadata_type_to_type, get_customschema_from_metadata
+import json
+
+s3_resource = boto3.resource('s3')
 
 def s3_path_to_bucket_key(path):
     path = path.replace("s3://", "")
@@ -11,3 +13,9 @@ def spark_read_csv_using_metadata_path(metadata_path, csv_path, **kwargs):
     schema = get_customschema_from_metadata(meta_json)
     df = spark.read.csv(csv_path, schema=schema, **kwargs)
     return df
+
+def read_json_from_s3(s3_path):
+    bucket, key = s3_path_to_bucket_key(s3_path)
+    obj = s3_resource.Object(bucket, key)
+    text = obj.get()['Body'].read().decode('utf-8')
+    return json.loads(text)
