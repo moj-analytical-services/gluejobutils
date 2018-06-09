6d5ddeba755742f33030923482f9799e75ab3c4b
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2018-06-09 10:04:54 +0100

    pass spark

--- gluejobutils/s3.py
+++ gluejobutils/s3.py
@@ -8,7 +8,7 @@ def s3_path_to_bucket_key(path):
     bucket, key = path.split('/', 1)
     return bucket, key
 
-def spark_read_csv_using_metadata_path(metadata_path, csv_path, **kwargs):
+def spark_read_csv_using_metadata_path(spark, metadata_path, csv_path, **kwargs):
     meta_json = read_json_from_s3(metadata_path)
     schema = get_customschema_from_metadata(meta_json)
     df = spark.read.csv(csv_path, schema=schema, **kwargs)
