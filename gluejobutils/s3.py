400235088a3fb39e4d4ba71aa604ae4db4fcb033
Karik Isichei <karik.isichei@gmail.com>
2018-08-20 09:50:14 +0100

    added descriptions to each function

--- gluejobutils/s3.py
+++ gluejobutils/s3.py
@@ -2,6 +2,7 @@ from gluejobutils.datatypes import translate_metadata_type_to_type, get_customsc
 from gluejobutils.utils import add_slash, remove_slash
 import json
 import boto3
+import botocore
 
 try :
     # python 2.7
@@ -11,6 +12,13 @@ except :
     from io import StringIO
 
 s3_resource = boto3.resource('s3')
+
+def bucket_key_to_s3_path(bucket, key) :
+    """
+    Takes an S3 bucket and key combination and returns the full S3 path to that location.
+    """
+    key = add_slash(key)
+    return 's3://{}/{}'.format(bucket, key)
 
 def s3_path_to_bucket_key(s3_path):
     """
@@ -47,14 +55,13 @@ def write_json_to_s3(data, s3_path) :
     return log_upload_resp
 
 
-def get_filepaths_from_s3_folder(s3_path, parquet = False) :
+def get_filepaths_from_s3_folder(s3_folder_path, parquet = False) :
     """
     Get a list of filepaths from a bucket. If parquet is set to True then only return a distinct list of folder
     paths that contain files with a .parquet extension.
     """
-
-    s3_path = add_slash(s3_path)
-    bucket, key = s3_path_to_bucket_key(s3_path)
+    s3_folder_path = add_slash(s3_folder_path)
+    bucket, key = s3_path_to_bucket_key(s3_folder_path)
 
     s3b = s3_resource.Bucket(bucket)
     obs = s3b.objects.filter(Prefix = key)
@@ -63,46 +70,57 @@ def get_filepaths_from_s3_folder(s3_path, parquet = False) :
     if parquet :
         ob_keys = list(set(["/".join(o.split('/')[:-1]) + '/' for o in ob_keys if o[-8:] == '.parquet']))
 
-    ob_keys = sorted([create_s3_path(bucket, o) for o in ob_keys])
+    ob_keys = sorted([bucket_key_to_s3_path(bucket, o) for o in ob_keys])
 
     return ob_keys
 
-def copy_s3_folder_contents_to_new_folder(old_folder_path, new_folder_path) :
-    old_folder_path = add_slash(old_folder_path)
-    new_folder_path = add_slash(new_folder_path)
+def copy_s3_folder_contents_to_new_folder(old_s3_folder_path, new_s3_folder_path) :
+    old_s3_folder_path = add_slash(old_s3_folder_path)
+    new_s3_folder_path = add_slash(new_s3_folder_path)
 
-    old_bucket, old_obj_key = s3_path_to_bucket_key(old_folder_path)
+    old_bucket, old_obj_key = s3_path_to_bucket_key(old_s3_folder_path)
     all_old_filepaths = get_filepaths_from_s3_folder(old_bucket, old_obj_key)
     for ofp in all_old_filepaths :
-        nfp = ofp.replace(old_folder_path, new_folder_path)
+        nfp = ofp.replace(old_s3_folder_path, new_s3_folder_path)
         copy_s3_object(ofp, nfp)
 
-# STOPPED HERE
-
-def delete_s3_folder_contents(bucket, folder_path) :
-    all_obs = get_filepaths_from_s3_bucket(bucket, folder_path)
-    for o in all_obs :
-        s3_resource.Object(bucket, o).delete()
+def delete_s3_folder_contents(s3_folder_path) :
+    bucket, folder_path = s3_path_to_bucket_key(s3_folder_path)
+    folder_path = add_slash(folder_path)
+    all_filepaths = get_filepaths_from_s3_folder(bucket, folder_path)
+    for f in all_filepaths :
+        b, o = s3_path_to_bucket_key(f)
+        s3_resource.Object(b, o).delete()
 
 def copy_s3_object(old_s3_path, new_s3_path) :
+    """
+    Copies a file in S3 from one location to another.
+    """
     new_bucket, new_key = s3_path_to_bucket_key(old_s3_path)
     s3_resource.Object(new_bucket, new_key).copy_from(CopySource=old_s3_path)
 
-def create_s3_path(bucket, object_path) :
-    object_path = add_slash(object_path)
-    return 's3://{}/{}'.format(bucket, object_path)
-
-def check_for_s3_file(bucket, file_key) :
+# https://stackoverflow.com/questions/33842944/check-if-a-key-exists-in-a-bucket-in-s3-using-boto3
+def check_for_s3_file(s3_path) :
+    """
+    Returns True/False depending if a file exists in the S3 path provided.
+    exists = check_for_s3_file(s3://my-bucket/file.txt)
+    """
+    bucket, key = s3_path_to_bucket_key(s3_path)
     try:
-        s3_resource.Object(bucket, file_key).load()
+        s3_resource.Object(bucket, key).load()
     except botocore.exceptions.ClientError as e:
         if e.response['Error']['Code'] == "404":
             return False
         else:
-            raise ValueError("Unknown Error: boto did not return expected error on checking if file exists")
+            raise
     else:
         # The object does exist.
         return True
 
-def check_for_parquet_file(bucket, prefix) :
-    return len(get_filepaths_from_s3_bucket(bucket, prefix, parquet = True)) == 1
+def check_for_parquet_data_in_folder(s3_folder_path) :
+    """
+    Checks if a folder contains parquet data. Used to test if parquet data is in s3 folder.
+    exists = check_for_parquet_data_in_folder("s3://my-bucket/data/")
+    """
+    s3_folder_path = add_slash(s3_folder_path)
+    return len(get_filepaths_from_s3_folder(s3_folder_path, parquet = True)) == 1
