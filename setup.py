c94ef31c7dab28d2c49ef8a4e5fcec64702cdfa5
Karik Isichei <karik.isichei@gmail.com>
2018-10-01 10:15:26 +0100

    updating setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 # see https://stackoverflow.com/questions/14399534/reference-requirements-txt-for-the-install-requires-kwarg-in-setuptools-setup-py
 setup(name='gluejobutils',
-      version='1.0.0',
+      version='1.0.1',
       description='Python 2.7 utils for glue jobs',
       long_description=open('README.md').read(),
       url='https://github.com/moj-analytical-services/gluejobutils',
