a1141c0d5d8bced6fdb9f8d5e703f96dd88c8fbd
Karik Isichei <karik.isichei@gmail.com>
2019-04-05 13:30:37 +0100

    Updating write_json_to_s3 function

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 # see https://stackoverflow.com/questions/14399534/reference-requirements-txt-for-the-install-requires-kwarg-in-setuptools-setup-py
 setup(name='gluejobutils',
-      version='1.0.2',
+      version='1.0.3',
       description='Python 2.7 utils for glue jobs',
       long_description=open('README.md').read(),
       url='https://github.com/moj-analytical-services/gluejobutils',
